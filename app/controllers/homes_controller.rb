0ed17dca7947b97ff5a876d62a7aae7ed489f666
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-22 17:55:00 +0200

    Redirect to categories on user sign in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,10 @@
 class HomesController < ApplicationController
   # GET /homes
-  def index; end
+  def index
+    respond_to do |format|
+      if user_signed_in?
+        format.html { redirect_to groups_path, notice: 'Logged in successfully' }
+      end
+    end
+  end
 end

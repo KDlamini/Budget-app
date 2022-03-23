096b8913a4f964d93310fd3cc6f8545b7da2243c
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-23 12:26:27 +0200

    Update records routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     root 'homes#index'
     resources :users, only:  [:index]
     resources :groups  do
-      resources :records,  only:  [:new, :create, :destroy]
+      resources :records,  only:  [:index, :new, :create, :destroy]
     end
   end
 end

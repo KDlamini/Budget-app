a7cb1cb203f0b6b3ff3169ac74d0c55836e18746
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-23 12:27:42 +0200

    Add record model and update group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
     has_one_attached :image
+
+    validates :name, presence: true
+    validates :image, presence: true
+
+    has_many :records, dependent: :delete_all
+    belongs_to :author, class_name: 'User'
 end

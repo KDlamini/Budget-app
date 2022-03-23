5be199aa9cc3d21e2e5b7f27e35bb8fc9cff2e12
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-23 12:25:49 +0200

    Update records schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_152554) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_162437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_152554) do
     t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
+  create_table "records", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_records_on_author_id"
+    t.index ["group_id"], name: "index_records_on_group_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -70,4 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_152554) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "records", "groups"
+  add_foreign_key "records", "users", column: "author_id"
 end

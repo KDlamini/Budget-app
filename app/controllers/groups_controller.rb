9df45be5c66c09bd871434d86f3b0b37845885d0
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-24 13:04:20 +0200

    Update show method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: %i[show edit update destroy]
+  before_action :set_group, only: %i[manage edit update destroy]
 
   # GET /groups
   def index
@@ -7,7 +7,9 @@ class GroupsController < ApplicationController
   end
 
   # GET /groups/1
-  def show; end
+  def show
+    @groups = current_user.groups
+  end
 
   # GET /groups/new
   def new
@@ -23,7 +25,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -34,7 +36,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -46,7 +48,7 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+      format.html { redirect_to groups_url, notice: 'Category was successfully destroyed.' }
     end
   end
 
